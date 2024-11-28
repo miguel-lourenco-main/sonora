daf0e5b6862827c49ad9ac0ab297380c617aa6f7
= <mdsllc603@gmail.com>
2024-11-28 02:27:30 +0000

    save progress

--- tooling/eslint/nextjs.js
+++ tooling/eslint/nextjs.js
@@ -1,8 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: ['plugin:@next/next/recommended'],
+  extends: ['plugin:@next/next/recommended', 'plugin:next-on-pages/recommended'],
+  plugins: ['next-on-pages'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
+    'next-on-pages/no-unsupported-configs': 'warn',
   },
 };
 
