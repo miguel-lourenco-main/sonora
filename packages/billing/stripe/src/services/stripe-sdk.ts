38f045b416359a5a57344ffad6b34c98bb0cbb82
Miguel Lourenco <mdsllc603@gmail.com>
2025-01-23 15:59:21 +0000

    feat: added logs to .env variables inside stripe-sdk

--- packages/billing/stripe/src/services/stripe-sdk.ts
+++ packages/billing/stripe/src/services/stripe-sdk.ts
@@ -10,6 +10,9 @@ import { StripeServerEnvSchema } from '../schema/stripe-server-env.schema';
 export async function createStripeClient() {
   const { default: Stripe } = await import('stripe');
 
+  console.log('process.env.STRIPE_SECRET_KEY', process.env.STRIPE_SECRET_KEY);
+  console.log('process.env.STRIPE_WEBHOOK_SECRET', process.env.STRIPE_WEBHOOK_SECRET);
+
   // Parse the environment variables and validate them
   const stripeServerEnv = StripeServerEnvSchema.parse({
     secretKey: process.env.STRIPE_SECRET_KEY,
